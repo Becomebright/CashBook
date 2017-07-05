6a9a95c0e10c205bd16a09f08a88720570bdc73d
dsz <694608794@qq.com>
2017-07-05 21:38:40 +0800

    Fitst commit

---
+++ app/src/main/java/com/example/cashbook/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.cashbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+
+    private EditText inputText;
+
+    private Button send;
+
+    private RecyclerView msgRecyclerView;
+
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initMsg();
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecyclerView =(RecyclerView) findViewById(R.id.msg_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    inputText.setText("");
+                }
+            }
+        });
+
+    }
+
+    private void initMsg() {
+        Msg msg1 = new Msg("零食 20", Msg.TYPE_SENT);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("少吃点，记得减肥！", Msg.TYPE_RECEIVED);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("交通 20",Msg.TYPE_SENT);
+        msgList.add(msg3);
+        Msg msg4 = new Msg("注意交通安全", Msg.TYPE_RECEIVED);
+        msgList.add(msg4);
+    }
+
+}
---
+++ app/src/main/java/com/example/cashbook/MsgAdapter.java
@@ -0,0 +1,72 @@
+package com.example.cashbook;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by dsz62 on 2017/7/5.
+ */
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+
+    private List<Msg> mMsgList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+        ImageView leftImg;
+        ImageView rightImg;
+
+        public ViewHolder(View view) {
+            super(view);
+            leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
+            rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
+            leftMsg = (TextView) view.findViewById(R.id.left_msg);
+            rightMsg = (TextView) view.findViewById(R.id.right_msg);
+            leftImg = (ImageView) view.findViewById(R.id.left_img);
+            rightImg = (ImageView) view.findViewById(R.id.right_img);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList) {
+        mMsgList = msgList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.msg_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Msg msg = mMsgList.get(position);
+        if (msg.getType() == Msg.TYPE_RECEIVED) {
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+            holder.leftImg.setImageResource(R.drawable.left_head_portrait);
+        }
+        else if (msg.getType() == Msg.TYPE_SENT) {
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.rightMsg.setText(msg.getContent());
+            holder.rightImg.setImageResource(R.drawable.right_head_portrait);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
+}
