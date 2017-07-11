124424e0a8760863315775b06650c7493e09c961
dsz <694608794@qq.com>
2017-07-11 14:53:13 +0800

    7/11 Afternoon 1
    新增删除历史数据时的确认提示
    优化HistoryBillActivity.java的结构

--- app/src/main/java/com/example/cashbook/history_bill/HistoryBillActivity.java
+++ app/src/main/java/com/example/cashbook/history_bill/HistoryBillActivity.java
@@ -1,5 +1,7 @@
 package com.example.cashbook.history_bill;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,39 +24,31 @@ public class HistoryBillActivity extends AppCompatActivity {
 
     private Button delete, backward;
 
+    private HistoryBillAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_bill);
 
-        //隐藏ActionBar
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.hide();
-        }
-
+        hideActionBar(); //隐藏ActionBar
 
         initBills();
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.history_bill);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        HistoryBillAdapter adapter = new HistoryBillAdapter(billList);
+        adapter = new HistoryBillAdapter(billList);
         recyclerView.setAdapter(adapter);
 
         delete = (Button) findViewById(R.id.history_delete);
         backward = (Button) findViewById(R.id.history_backward);
 
-        //用于清空数据库
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(HistoryBillActivity.this, "Delete database", Toast.LENGTH_SHORT).show();
-                DataSupport.deleteAll(Consumption.class);
+        deleteAllButton();//用于清空数据库
 
-            }
-        });
+        backButton(); //返回上一界面
+    }
 
-        //返回上一界面
+    private void backButton() {
         backward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,7 +56,43 @@ public class HistoryBillActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    private void deleteAllButton() {
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(HistoryBillActivity.this);
+                dialog.setTitle("确定要删除所有支出信息么？");
+                dialog.setMessage("一旦确认删除，支出信息将无法恢复");
+                dialog.setCancelable(false); //无法通过点击外部取消
+                dialog.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DataSupport.deleteAll(Consumption.class);
+
+                        billList.clear();
+                        adapter.notifyDataSetChanged();
+
+                        Toast.makeText(HistoryBillActivity.this, "删除完毕", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+
+    private void hideActionBar() {
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
     }
 
     //从数据库中读取数据，初始化billList
